b546e8e46100775199024f6f90167c5311e1f082
chko182 <chko.martinez@gmail.com>
2016-05-18 22:27:07 -0500

    Update AppDelegate.h

--- src/ios/AppDelegate.h
+++ src/ios/AppDelegate.h
@@ -25,7 +25,7 @@
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
 //
 
-#import <Cordova/CDVViewController.h>
+#import "CDVViewController.h"
 #import "CDVAppDelegate.h"
 
 #define kAppScheme @"ahmsaplugin://"
